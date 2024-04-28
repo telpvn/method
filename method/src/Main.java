d59f343aefddf40909483f13bf6da4b459e9efd6
telpvn <telpvn@mail.ru>
2024-04-28 16:00:47 +0400

    old code robot 1

--- method/src/Main.java
+++ method/src/Main.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[] array = {1, 2, 3, 4, 5, 6, 7};
+    static int x = 0;
+    static int y = 0;
 
     public static void main(String[] args) {
-        printArray();
-
-
-        array[2] = inputNum();
-
-        printArray();
-    }
-
-    public static void printArray() {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        while (true) {
+            System.out.println("Введите команду:\n1-пойти вниз\n2-пойти вверх\n3-пойти направо\n4-пойти налево\n5-печать координат робота:");
+            int command = new Scanner(System.in).nextInt();
+            if (command == 1) {
+                y--;
+            } else if (command == 2) {
+                y++;
+            } else if (command == 3) {
+                x++;
+            } else if (command == 4) {
+                x--;
+            } else if (command == 5) {
+                System.out.println("Координаты: x " + x + ", y " + y);
+            } else {
+                System.out.println("Команда не верна!");
+            }
         }
-    }
-
-    public static int inputNum() {
-        System.out.println("Введите число");
-        return new Scanner(System.in).nextInt();
     }
 }
