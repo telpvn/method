ff492decad956e2a773aede17e6e527317110bec
telpvn <telpvn@mail.ru>
2024-04-28 13:07:56 +0400

    Method code

--- method/src/Main.java
+++ method/src/Main.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-   static int[] array = {1, 2, 3, 4, 5, 6, 7};
+    static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-        System.out.println("Введите число");
-        int input = new Scanner(System.in).nextInt();
-        array[2] = input;
+        printArray();
 
+
+        array[2] = inputNum();
+
+        printArray();
+    }
+
+    public static void printArray() {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
+    }
+
+    public static int inputNum() {
+        System.out.println("Введите число");
+        return new Scanner(System.in).nextInt();
     }
 }
