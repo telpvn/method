fba4b2c76c9964dc59db590e49c9dcf38aa792de
telpvn <telpvn@mail.ru>
2024-04-29 13:38:59 +0400

    Library

--- method/src/Main.java
+++ method/src/Main.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    static String booksInfo = "";
+
     public static void main(String[] args) {
-        int a = 0;
-        test(a, 0.0, "test", 0, 0);
-        test2(0, 0, 0, 0, 0);
-
+        while (true) {
+            String name = inputBookName();
+            int pageCount = inputPageCount();
+            addBook(name);
+            addBook(name, pageCount);
+            printInfo();
+        }
     }
 
-    public static void test(int a, double b, String c, int d, int e) {
-        System.out.println(a + b + c + d + e);
+    public static String inputBookName() {
+        System.out.println("Введите название книги:");
+        return new Scanner(System.in).nextLine();
     }
 
-    public static void test2(int f, int g, int h, int i, int j) {
-        System.out.println(f - g - h - i - j);
+    public static int inputPageCount() {
+        System.out.println("Введите кол-во страниц: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void addBook(String bookName) {
+        addBook(bookName, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+    }
+
+    public static void printInfo() {
+        System.out.println(booksInfo);
     }
 }
