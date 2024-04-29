7e0d62982009fa3e980f1987fdb5141d38daf8ea
telpvn <telpvn@mail.ru>
2024-04-29 12:39:32 +0400

    Methods with parameters

--- method/src/Main.java
+++ method/src/Main.java
@@ -1,26 +1,16 @@
 public class Main {
-    static int a;
-    static int b;
-    static int c;
-    static int d;
-    static int e;
-
-    static int f;
-    static int g;
-    static int h;
-    static int i;
-    static int j;
-
     public static void main(String[] args) {
-        test();
-        test2();
+        int a = 0;
+        test(a, 0.0, "test", 0, 0);
+        test2(0, 0, 0, 0, 0);
 
     }
 
-    public static void test() {
+    public static void test(int a, double b, String c, int d, int e) {
         System.out.println(a + b + c + d + e);
     }
-    public static void test2() {
+
+    public static void test2(int f, int g, int h, int i, int j) {
         System.out.println(f - g - h - i - j);
     }
 }
